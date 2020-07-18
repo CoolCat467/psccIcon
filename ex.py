8dbc82519d4edd25096c0ab90d077ba1f3d69846
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2020-07-18 12:46:15 -0500

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,15 +1,18 @@
+#!/usr/bin/env python3
+# Modify file data
+# -*- coding: utf-8 -*-
 
-fs=open('PSIconsLowRes_.dat','rb')
-stm=fs.read()
+fs = open('PSIconsLowRes_.dat','rb')
+stm = fs.read()
 fs.close()
 
-i=4
+i = 4
 while True:
-    p=stm.find(b'IEND',i)
-    if p!=-1:
-        fs=open('Low2_\\%08X.png'%i,'wb')
+    p = stm.find(b'IEND', i)
+    if p != -1:
+        fs = open('Low2_\\%08X.png' % i, 'wb')
         fs.write(stm[i:p+8])
-        i=p+8
+        i = p+8
         fs.close()
     else:
         break
