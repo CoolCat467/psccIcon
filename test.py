447f2bdb725357b3cfce45e58d9cd56b0a13172d
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2020-07-18 13:30:39 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
+#!/usr/bin/env python3
+# Icon Resources Idx file reader
+# -*- coding: utf-8 -*-
+
 import re
-import struct
-
-def GetStruct(ss):
-    s=[]
-    pos=0
-    for i in range(40):
-        s.append(int(ss[pos:pos+8],16))
-        pos+=8
-    return s
 
-pat=re.compile(r'[^:]+: "([ABCDEF0123456789]*)".*')
+def getStruct(ss, size=8, base=16, rng=40):
+    return [int(ss[i * size:(i + 1) * size], base) for i in range(rng)]
 
-ls=open('IconResources.idx','rb').read().decode().split('\r\n')
+def run():
+    pattern = re.compile(r'[^:]+: "([ABCDEF0123456789]*)".*')
 
-for l in ls:
-    mo=pat.match(l)
-    if mo:
-        s=GetStruct(mo.group(1))
-        for i in range(len(s)):
-            if ((i%4==2) or (i%4==3)): #and ((i%20!=2) and (i%20!=3)):
-                if s[i]!=0:
-                    print(l)
-                    break
-        
+    lines = open('IconResources.idx', 'rb').read().decode().split('\r\n')
+
+    for line in lines:
+        match = pattern.match(line)
+        if match:
+            struct = getStruct(match.group(1))
+            for i in range(len(struct)):
+                if (i%4 == 2) or (i%4 == 3):#and ((i%20!=2) and (i%20!=3)):
+                    if struct[i] != 0:
+                        print(line)
+                        break
+##if __name__ == '__main__':
+run()
